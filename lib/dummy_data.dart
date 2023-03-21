b102b85f4dca5a6105bfbd8a4a51e346c0b52565
Mayuresh Gavai <blackoutmayuresh2912@gmail.com>
2023-03-22 03:01:38 +0530

    first commit

---
+++ lib/dummy_data.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'models/category.dart';
+
+List<Category> Dummy_Categories = [
+  Category(id: 'c1', title: 'Indian', color: Colors.blue),
+  Category(id: 'c2', title: 'Quick & Easy', color: Colors.redAccent),
+  Category(id: 'c3', title: 'Breakfast', color: Colors.green),
+  Category(id: 'c4', title: 'Snacks', color: Colors.indigoAccent),
+  Category(id: 'c5', title: 'Burgurs', color: Colors.brown),
+  Category(id: 'c6', title: 'Light & Lovely', color: Colors.purple),
+];
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import './categories.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'recipe_app',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('DailyMeals'),
+      ),
+      body: CategoriesScreen(),
+    );
+  }
+}
